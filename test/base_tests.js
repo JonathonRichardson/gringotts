545bbb985a94556366653d0881937c335d941f7e
Jon Richardson <jonathon.m.richardson@gmail.com>
2016-06-08 18:21:54 -0500

    Add some more tests to verify functionality.

--- test/base_tests.js
+++ test/base_tests.js
@@ -1,16 +1,23 @@
 var fs = require("fs");
 var path = require("path");
 var child_process = require("child_process");
+var _ = require("underscore");
 
 var cwd = process.cwd();
 var test_dir = path.join(cwd, "test_files");
 var testdbfile = path.join(test_dir, "test.db")
 
+var execConfig = {
+  cwd: test_dir
+};
+
+var exec = function(command) {
+  return child_process.execSync(command, execConfig).toString();
+}
+
 describe("dbctl", function() {
   var dbctl = function(command, file) {
-    return child_process.execSync('../target/debug/dbctl ' + command + ' ' + file, {
-      cwd: test_dir
-    })
+    return exec('../target/debug/dbctl ' + command + ' ' + file);
   };
 
   beforeAll(function() {
@@ -19,12 +26,70 @@ describe("dbctl", function() {
 
   afterAll(function() {
     fs.rmdirSync(test_dir);
-  })
+  });
 
-  it("should be able to create the db file", function() {
-    dbctl('create', testdbfile);
+  describe("create", function() {
+    beforeAll(function() {
+      dbctl('create', testdbfile);
+    });
 
-    var fileStat = fs.statSync(testdbfile); // This will throw if it doesn't exist
-    fs.unlinkSync(testdbfile);
+    afterAll(function() {
+      fs.unlinkSync(testdbfile);
+    });
+
+    it("should be able to create the db file", function() {
+      var fileStat = fs.statSync(testdbfile); // This will throw if it doesn't exist
+    });
+
+    it("should start with the magic string", function() {
+      var magic_string = exec('head -c 65 ' + testdbfile);
+      expect(magic_string).toBe("GringottsDBFile - https://github.com/JonathonRichardson/gringotts");
+    });
+
+    it("should set the first block's header line", function() {
+      var output = exec("xxd " + testdbfile + " | grep -E '^00001' | tr -d '\n'");
+      expect(output).toMatch(/: 0000 0000 1600/);
+    });
+
+    xit("should not create the file twice", function() {
+      var output = dbctl("create", testdbfile);
+
+      expect(output).not.toMatch(/Successfully created/i);
+    });
+  });
+
+  describe("info", function() {
+    beforeAll(function() {
+      dbctl('create', testdbfile);
+    });
+
+    afterAll(function() {
+      fs.unlinkSync(testdbfile);
+    });
+
+    it("should display the correct initial information.", function() {
+      var output = dbctl("info", testdbfile);
+      var unexecuted_expects = 3;
+
+      _.each(output.split(/\n/), function(line) {
+        var key = line.split(/:\s+/)[0];
+        var val = line.split(/:\s+/)[1];
+
+        if (key.match(/block size/i)) {
+          expect(val).toBe("4kb");
+          unexecuted_expects--;
+        }
+        else if (key.match(/version/i)) {
+          expect(val).toBe("0.0.1");
+          unexecuted_expects--;
+        }
+        else if (key.match(/number of blocks/i)) {
+          expect(val).toBe("1");
+          unexecuted_expects--;
+        }
+      });
+
+      expect(unexecuted_expects).toBe(0);
+    });
   });
 });
