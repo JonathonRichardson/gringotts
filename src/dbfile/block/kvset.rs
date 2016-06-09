d89d21541c4e4f09095aaf0e0ca90defe212827d
Jon Richardson <jonathon.m.richardson@gmail.com>
2016-06-09 15:36:51 -0500

    Convert KVSet to use a BTreeMap.

--- src/dbfile/block/kvset.rs
+++ src/dbfile/block/kvset.rs
@@ -1,4 +1,4 @@
-use std::collections::LinkedList;
+use std::collections::BTreeMap;
 use std::str;
 use error::*;
 
@@ -8,36 +8,34 @@ pub struct KVPair {
 }
 
 pub struct KVSet {
-    pairs: LinkedList<KVPair>
+    pairs: BTreeMap<String, String>
 }
 
 impl KVSet {
     pub fn new() -> KVSet {
         return KVSet {
-            pairs: LinkedList::new()
+            pairs: BTreeMap::new()
         }
     }
 
     /// Adds a new key/value pair to the KVSet.  The return value will be a String, if there was
     /// a previous value and this is therefore an update, or None, if this was a true insert.
-    /*
-    pub fn put(key: String, value: String) -> Option<String> {
-
+    pub fn put(&mut self, key: String, value: String) -> Option<String> {
+        return self.pairs.insert(key, value); // this returns the old value or None
     }
 
-    pub fn get(key: String) -> Option<String> {
-
+    pub fn get(&self, key: String) -> Option<&String> {
+        return self.pairs.get(&key);
     }
-    */
 
     pub fn deserialize(bytes: &mut Vec<u8>) -> Result<KVSet, CorruptDataError> {
-        let mut list = LinkedList::new();
+        let mut map = BTreeMap::new();
         let length = bytes.len();
 
         // If the byte vector is empty, we're good.
         if (length == 0) {
             return Ok(KVSet {
-                pairs: list,
+                pairs: map,
             });
         }
 
@@ -59,11 +57,7 @@ impl KVSet {
                 match bytes[i] {
                     0 => {
                         let val = String::from_utf8(utf8_buffer.clone()).unwrap(); // TODO: change unwrap to throw CorruptDataError
-
-                        list.push_back(KVPair {
-                            key: cur_key.clone(),
-                            value: val,
-                        });
+                        map.insert(cur_key.clone(), val);
                         utf8_buffer.clear();
                     },
                     1 => {
@@ -81,20 +75,20 @@ impl KVSet {
         }
 
         return Ok(KVSet {
-            pairs: list
+            pairs: map
         });
     }
 
     pub fn serialize(&self) -> Vec<u8> {
         let mut bytes = Vec::new();
 
-        for kvpair in self.pairs.iter() {
+        for key in self.pairs.keys() {
             if (bytes.len() > 0) {
                 bytes.push(0);
                 bytes.push(0);
             }
 
-            for byte in kvpair.key.clone().into_bytes() {
+            for byte in key.clone().into_bytes() {
                 if (byte == 0) {
                     bytes.push(0);
                     bytes.push(2);
@@ -107,7 +101,7 @@ impl KVSet {
             bytes.push(0);
             bytes.push(1);
 
-            for byte in kvpair.value.clone().into_bytes() {
+            for byte in self.pairs.get(key).unwrap().clone().into_bytes() {
                 if (byte == 0) {
                     bytes.push(0);
                     bytes.push(2);
@@ -125,35 +119,25 @@ impl KVSet {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::collections::LinkedList;
+    use std::collections::BTreeMap;
 
 
     #[test]
     fn basic_serialization() {
-        let mut list: LinkedList<KVPair> = LinkedList::new();
+        let mut keyset = KVSet::new();
+        keyset.put(String::from("yes"),   String::from("no"));
+        keyset.put(String::from("hello"), String::from("goodbye"));
 
-        list.push_back(KVPair {
-            key: String::from("yes"),
-            value: String::from("no")
-        });
-
-        list.push_back(KVPair {
-            key: String::from("hello"),
-            value: String::from("goodbye")
-        });
-
-        let keyset = KVSet {
-            pairs: list
-        };
-
-        assert_eq!(keyset.serialize(), vec!(
+        let serialized_vector = vec!(
+            104, 101, 108, 108, 111,
+            0, 1,
+            103, 111, 111, 100, 98, 121, 101,
+            0, 0,
             121, 101, 115,
             0, 1,
             110, 111,
-            0, 0,
-            104, 101, 108, 108, 111,
-            0, 1,
-            103, 111, 111, 100, 98, 121, 101
-         ));
+        );
+
+        assert_eq!(keyset.serialize(), serialized_vector);
     }
 }
